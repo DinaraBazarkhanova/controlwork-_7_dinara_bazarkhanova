779671fdfee427408d933612d2a42a4739d3dfcc
Dinara Bazarkhanova <dikosha8.00@gmail.com>
2023-02-25 12:02:39 +0100

    Изменила файл urls.py

--- source/webapp/urls.py
+++ source/webapp/urls.py
@@ -1,10 +1,13 @@
 from django.urls import path
 
-from webapp.views.articles import add_view
+from webapp.views.articles import add_view, update_view, delete_view
 from webapp.views.base import index_view
 
 urlpatterns = [
     path('', index_view, name='index'),
     path('article/', index_view),
     path('article/add/', add_view, name='article_add'),
+    path('article/<int:pk>/update', update_view, name='article_update'),
+    path('article/<int:pk>/delete', delete_view, name='article_delete'),
+    path('article/<int:pk>/confirm_delete', confirm_delete, name='confirm_delete'),
 ]
