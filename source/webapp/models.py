7c36ade3062ec869c886ac4c1a03d55c32f0614c
Dinara Bazarkhanova <dikosha8.00@gmail.com>
2023-02-25 12:04:40 +0100

    Изменила файл models.py

--- source/webapp/models.py
+++ source/webapp/models.py
@@ -9,18 +9,20 @@ class StatusChoice(TextChoices):
 
 
 class Article(models.Model):
-    name = models.CharField(max_length=100, null=False, blank=False, verbose_name="Name")
+    name = models.CharField(max_length=100, null=False, blank=False, verbose_name="Имя")
     email = models.EmailField(max_length=50, null=False, blank=True, verbose_name="Email")
-    text = models.TextField(max_length=200, null=False, blank=False, verbose_name="Text")
-    created_at = models.DateTimeField(auto_now_add=True, verbose_name="Creation date and time")
-    updated_at = models.DateTimeField(auto_now=True, verbose_name="Updated date and time")
+    text = models.TextField(max_length=200, null=False, blank=False, verbose_name="Текст")
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name="Дата и время создания")
+    updated_at = models.DateTimeField(auto_now=True, verbose_name="Дата и время обновления")
     status = models.CharField(max_length=20,
                               null=False,
                               blank=False,
                               choices=StatusChoice.choices,
                               default=StatusChoice.ACTIVE,
-                              verbose_name="Status"
+                              verbose_name="Статус"
                               )
+    is_deleted = models.BooleanField(verbose_name='удалено', null=False, default=False)
+    deleted_at = models.DateTimeField(verbose_name='Дата и время удаления', null=True, default=None)
 
     def __str__(self):
         return f"{self.name} - {self.status}"
